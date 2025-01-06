963088e90b14edd1cc70e5f7993b7c1ec145e5c6
gargDhruv12 <dhruvgarg729729@gmail.com>
2025-01-06 18:57:47 +0530

    About section completed

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -43,51 +43,15 @@ import ReactLogo from '../components/ReactLogo'
 import Cube from '../components/Cube'
 import Ring from '../components/Ring'
 import HeroCamera from '../components/HeroCamera'
+import Button from '../components/Button'
 const Hero = () => {
-    // const x = useControls('Hacker Room', {
-    //     positionX: {
-    //         value: 2.5,
-    //         min: -10,
-    //         max: 10
-    //     },
-    //     positionY: {
-    //         value: 2.5,
-    //         min: -10,
-    //         max: 10
-    //     },
-    //     positionZ: {
-    //         value: 2.5,
-    //         min: -10,
-    //         max: 10
-    //     },
-    //     rotationX: {
-    //         value: 0,
-    //         min: -10,
-    //         max: 10
-    //     },
-    //     rotationY: {
-    //         value: 10,
-    //         min: -10,
-    //         max: 10
-    //     },
-    //     rotationZ: {
-    //         value: 0,
-    //         min: -10,
-    //         max: 10
-    //     },
-    //     scale: {
-    //         value: 1,
-    //         min: 0.1,
-    //         max: 10
-    //     }
-    // })
     const isSmall = useMediaQuery({ maxWidth: 440 })
     const isMobile = useMediaQuery({ maxWidth: 768 })
     const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 })
     const sizes = calculateSizes(isSmall, isMobile, isTablet);
     return (
         <section className='min-h-screen w-full flex flex-col relative'>
-            <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
+            <div className='w-full mx-auto flex flex-col sm:mt-30 mt-20 c-space gap-3'>
                 <p className='sm: text-3xl text-xl font-medium text-white text-center font-generalsans'>Hi, I am Dhruv <span className='waving-hand'>👋</span></p>
                 <p className='hero_tag text-gray_gradient text-center'>Full Stack Developer</p>
             </div>
@@ -117,6 +81,11 @@ const Hero = () => {
 
                 </Canvas>
             </div>
+            <div className='absolute bottom-7 left-0 right=0 w-full z-10 c-space'>
+                <a href="#contact" className='w-fit'>
+                    <Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96 " />
+                </a>
+            </div>  
         </section>
     )
 }
